8340b31d0479af2ea3fd1ed97ad9c8da81d33f02
garsan84 <jgrsan84@gmail.com>
2021-09-21 22:56:15 +0200

    display add comments

---
+++ main.js
@@ -0,0 +1,43 @@
+let add = () => {
+    
+    let inputName = document.getElementById('input-name');
+    let inputTextArea = document.getElementById('input-textarea');
+
+    if (inputName.value !== "" && comment.value != "") {
+       
+        let comment = document.querySelector('#comment');
+
+        let  comment_item = document.createElement('l1');
+
+        comment_item.innerHTML = `
+            <span>
+            <p>${ inputName.value}</p>
+            </span>
+            <p>${inputTextArea.value}</p> `;
+        comment.append(comment_item);
+    }
+
+
+if (inputName.value == "" || inputTextArea.value == "") {
+    let list = document.querySelector('.list'); 
+    let list_item = document.createElement ("l2"); 
+ 
+    list.append(list_item); 
+     }
+     inputName.value = comment.value= "";
+     inputTextArea.value= comment.value = "";
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
